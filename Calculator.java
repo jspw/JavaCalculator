a8bfc10ee984d6d6e0f7a831ea0c35577690169d
MH_Shifat <mehedi17@student.sust.edu>
2019-02-20 03:28:11 +0600

    Done only for + - * /

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javalectures;
+
+/**
+ *
+ * @author mhshi
+ */
+public class Calculator {
+    int a;
+    int b;
+    
+    int addition(int x,int y)
+    {
+        int sum = x+y;
+        return sum;
+    }
+}
